69a386335d83ed7403dfa8e8e410b14be6e3ef8d
Ahneb <benjaminha1996@gmail.com>
2023-02-07 10:02:40 -0800

    capitalized folder name to work on heroku (in the router methods)

--- Develop/controllers/apis/notes.js
+++ Develop/controllers/apis/notes.js
@@ -3,26 +3,26 @@ const fs = require("fs");
 const notesRouter = new Router();
 
 notesRouter.get('/', (req, res) => {
-    const notes = JSON.parse(fs.readFileSync('./develop/db/db.json', 'utf-8'));
+    const notes = JSON.parse(fs.readFileSync('./Develop/db/db.json', 'utf-8'));
     res.send(notes);
   });
 
 notesRouter.post('/', (req, res) => {
-    const data = fs.readFileSync('./develop/db/db.json');
+    const data = fs.readFileSync('./Develop/db/db.json');
     const notes = JSON.parse(data);
     notes.push(req.body);
     console.log(notes);
-    fs.writeFileSync('./develop/db/db.json', JSON.stringify(notes));
+    fs.writeFileSync('./Develop/db/db.json', JSON.stringify(notes));
     res.sendStatus(201);
   });
 
 notesRouter.delete('/:id', (req, res) => {
     const id = req.params.id;
-    let notes = JSON.parse(fs.readFileSync('./develop/db/db.json', 'utf8'));
+    let notes = JSON.parse(fs.readFileSync('./Develop/db/db.json', 'utf8'));
 
     notes = notes.filter(note => note.id !== id);
 
-    fs.writeFileSync('./develop/db/db.json', JSON.stringify(notes));
+    fs.writeFileSync('./Develop/db/db.json', JSON.stringify(notes));
     res.end();
   });
 
